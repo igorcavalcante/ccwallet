280fd27eb527da05a1f5b2a00f69fbcb02524400
Igor Cavalcante <igor.cavalcante@b2wdigital.com>
2017-08-21 20:45:30 -0300

    setup ebean

--- src/main/kotlin/br/eti/cavalcante/ccwallet/model/User.kt
+++ src/main/kotlin/br/eti/cavalcante/ccwallet/model/User.kt
@@ -1,4 +1,16 @@
 package br.eti.cavalcante.ccwallet.model
 
+import io.ebean.Model
+import javax.persistence.Entity
+import javax.persistence.Id
 
-class User() {}
+
+@Entity
+class User(val name: String) : Model() {
+
+    @Id
+    var id: Long = 0
+
+    var version: Long = 0
+
+}
--- src/test/kotlin/br/eti/cavalcante/ccwallet/UserTest.kt
+++ src/test/kotlin/br/eti/cavalcante/ccwallet/UserTest.kt
@@ -1,5 +1,26 @@
 package br.eti.cavalcante.ccwallet
 
+import br.eti.cavalcante.ccwallet.model.User
+import br.eti.cavalcante.ccwallet.model.query.QUser
+import io.ebean.Ebean
+import org.amshove.kluent.`should equal to`
+import org.testng.annotations.Test
+
 
 class UserTest {
+
+    @Test
+    fun save() {
+        val user = User("Igor")
+
+        Ebean.save(user)
+
+
+        1 `should equal to` 1
+
+        val const = QUser().name.eq("Igor").findOneOrEmpty()
+
+        const.ifPresent { it.name.`should equal to`("Igor") }
+    }
+
 }
